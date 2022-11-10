2aa7f2060bef4cf4385ca8b08f613f24f026fe21
Aston <94510172+atacoiguess@users.noreply.github.com>
2022-11-10 14:13:55 +1000

    Update Uuid.js

--- Uuid.js
+++ Uuid.js
@@ -2,9 +2,10 @@
  ////Retrieve quiz uuid from name////
 ////////////////////////////////////
 
-async function getQuizUUID(name) {
+async function getQuizUUID() {
+    var name = prompt("enter name")
     function sendEvent(uuid) {
-       window.dispatchEvent(new CustomEvent("uuid",{detail:uuid}));
+       quiz(uuid);
     }
     await fetch("https://kahoot.it/rest/kahoots/?query=" + name + "&limit=1").then((promise) => promise.json()).then((josn) => josn.entities[0].card.uuid).then((uuid) => sendEvent(uuid));
 }
@@ -13,12 +14,5 @@ async function getQuizUUID(name) {
  /////Now Ask User For Quiz Name////
 ///////////////////////////////////
 window.addEventListener("QuizStart",function() {
-    var name = prompt("Enter Quiz Name");
-    function init(event) {
-        var uuid = event.detail;
-        console.log(uuid)
-        quiz(uuid);
-    }
-    window.addEventListener("uuid",init);
-    getQuizUUID(name);
+    getQuizUUID();
 });
